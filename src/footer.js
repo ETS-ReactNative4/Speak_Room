fcb8103cf55368bf29c9604177c7dfd22b7c8103
Anton Kuts <a.kuts.ibg@gmail.com>
2019-01-14 20:29:22 +0200

    >1100px

--- src/footer.js
+++ src/footer.js
@@ -6,7 +6,7 @@ import inst from './img/inst.png';
 const Footer = () => (
   <footer>
     <div className="Footer-box">
-      <p>© 2018 - 2019 Speak room. Все права защищены</p>
+      <p>© 2012 - 2019 Speak room. Все права защищены</p>
       <div className="Footer-social-box">
         <a href="https://www.facebook.com/speakroomua/"><img src={fb} alt="fb"/></a>
         <a href="https://instagram.com/speakroom"><img src={inst} alt="inst"/></a>
